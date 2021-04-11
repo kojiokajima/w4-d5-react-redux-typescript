6bea5633379402a99a42fb0dc311f5be6ad18ff1
wanko <kouji124@w4.dion.ne.jp>
2021-04-11 12:14:59 -0700

    right before redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom'
 import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
-import {App} from './components/App'
+// import {App} from './components/App'
 
-const store = createStore(reducers, applyMiddleware(thunk))
+// const store = createStore(reducers, applyMiddleware(thunk))
+
+interface AppProps {
+  color?: string
+}
+
+const App = (props: AppProps): JSX.Element => {
+  return (
+    <div>{props.color}</div>
+  )
+}
 
 ReactDOM.render(
-  <Provider store={store}></Provider>,
+  // <Provider store={store}></Provider>,
+  <App color="red" ></App>,
   document.querySelector('#root')
 )
+
